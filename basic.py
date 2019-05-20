5ceac07b0bcde96711fcd6037dc4954f5213429a
sociopath00 <akshaynevrekar@gmail.com>
2019-05-20 20:25:52 +0530

    First Commit

---
+++ basic.py
@@ -0,0 +1,29 @@
+import logging
+
+
+LOGFILE = "main.log"
+
+logger = logging.getLogger("basic")
+
+# set logging level : INFO, DEBUG, WARNING or ERROR
+logger.setLevel(logging.DEBUG)
+
+# Create FileHandler with log file name
+file_handler = logging.FileHandler(LOGFILE)
+
+# This is the format in which logs will be displayed in log file
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
+# assign the formatter to file_handler object
+file_handler.setFormatter(formatter)
+
+# add the handler to logger
+logger.addHandler(file_handler)
+
+
+a = [1,2,3,4]
+
+logger.debug("The value of a: {}".format(a))
+
+for i, j in enumerate(a, start=1):
+	logger.debug("value {}: {}".format(i, j))
