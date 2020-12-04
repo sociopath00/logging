a0d6ec52b84178a4fe415b516810a4bedf1d202b
Akshay Nevrekar <akshaynevrekar@gmail.com>
2020-12-04 19:26:54 +0530

    Update daily_logs.py

--- daily_logs.py
+++ daily_logs.py
@@ -11,8 +11,8 @@ logger.setLevel(logging.DEBUG)
 
 
 # Create TimedRotatingFileHandler with log file name
-# It will create a new log file each day at midnight('m' stands for midnight)
-handler = TimedRotatingFileHandler(LOGFILE, when="m", interval=1)
+# It will create a new log file each day at midnight
+handler = TimedRotatingFileHandler(LOGFILE, when="midnight", interval=1)
 
 # This is the format in which logs will be displayed in log file
 formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
